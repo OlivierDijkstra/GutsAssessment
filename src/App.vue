ff8fd35a799a0f27909d77b8c1515efa18028a0e
Olivier Dijkstra <olivierdijkstra@gmail.com>
2021-10-08 18:09:19 +0200

    feat: initial commit
    fresh installation of vue 2, tailwind, commitizen & eslint

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Test msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Test,
+  },
+};
+</script>
